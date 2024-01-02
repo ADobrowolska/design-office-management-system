181b134b2c563c9804fc56dbe24e865bebf29cc4
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-01-02 20:09:17 +0100

    fc

---
+++ src/main/java/com/designofficems/designofficemanagementsystem/DesignOfficeManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.designofficems.designofficemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignOfficeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignOfficeManagementSystemApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/designofficems/designofficemanagementsystem/DesignOfficeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designofficems.designofficemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignOfficeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
