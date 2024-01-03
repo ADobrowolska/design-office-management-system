6ed8085115fc968394e902d11e31d5de76b80a91
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-01-03 19:07:24 +0100

    configure JwtAuthentication

---
+++ src/main/java/com/designofficems/designofficemanagementsystem/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.designofficems.designofficemanagementsystem.repository;
+
+import com.designofficems.designofficemanagementsystem.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
---
+++ src/main/java/com/designofficems/designofficemanagementsystem/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.designofficems.designofficemanagementsystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return null;
+    }
+}
