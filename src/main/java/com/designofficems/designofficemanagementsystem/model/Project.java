da6f3faecfdc1dfc83fae313b9bed4429a87dc97
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-01-07 21:30:35 +0100

    add project entity

---
+++ src/main/java/com/designofficems/designofficemanagementsystem/model/Project.java
@@ -0,0 +1,29 @@
+package com.designofficems.designofficemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "project")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private BigDecimal budget;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+
+}
