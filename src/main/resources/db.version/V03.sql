4bdb2c5723b47d3af989effdb00349c818f60292
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-01-07 21:30:23 +0100

    project table declaration

---
+++ src/main/resources/db.version/V03.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset adobrowolska:3
+
+CREATE TABLE project (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    budget DECIMAL NOT NULL,
+    description TEXT,
+    employee_id INT NOT NULL
+);
+
+ALTER TABLE project
+    ADD CONSTRAINT employee_id_fk
+    FOREIGN KEY (employee_id) REFERENCES employee(id);
+
