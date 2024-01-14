c42149db92b6b8f8010ebd05663b6a8fdb182940
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-01-14 13:36:40 +0100

    change Swagger configuration

--- src/main/java/com/designofficems/designofficemanagementsystem/configuration/swagger/OpenAPIConfiguration.java
+++ src/main/java/com/designofficems/designofficemanagementsystem/configuration/swagger/OpenAPIConfiguration.java
@@ -1,6 +1,9 @@
 package com.designofficems.designofficemanagementsystem.configuration.swagger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,8 +14,10 @@ import org.springframework.context.annotation.Configuration;
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer")
+@OpenAPIDefinition(info = @Info(title = "Design Office Management System"),
+        security = @SecurityRequirement(name = "Bearer Authentication"))
 public class OpenAPIConfiguration {
 
 
-    }
+}
 
