5cd67b45e22f5674355931689b164e4c76118d9c
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-01-21 22:32:29 +0100

    add endpoint getEmployeeRate with param

--- src/main/java/com/designofficems/designofficemanagementsystem/controller/EmployeeRateController.java
+++ src/main/java/com/designofficems/designofficemanagementsystem/controller/EmployeeRateController.java
@@ -6,6 +6,7 @@ import com.designofficems.designofficemanagementsystem.facade.EmployeeRateFacade
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,11 +21,14 @@ public class EmployeeRateController {
         this.employeeRateFacede = employeeRateFacede;
     }
 
-    @GetMapping("/employeerates")
+    @GetMapping("/employee/rate")
     public ResponseEntity<List<EmployeeRateDTO>> getEmployeeRate() {
         return ResponseEntity.ok(EmployeeRateMapper.mapToEmployeeRateDTOs(employeeRateFacede.getEmployeeRate()));
     }
 
-
+    @GetMapping("/employee/{id}/rate")
+    public ResponseEntity<List<EmployeeRateDTO>> getEmployeeRate(@PathVariable Integer employeeId) {
+        return ResponseEntity.ok(EmployeeRateMapper.mapToEmployeeRateDTOs(employeeRateFacede.getEmployeeRate(employeeId)));
+    }
 
 }
