bab305175e4e5cca1ad4b29645e8868ddc3148ed
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-01-24 18:53:51 +0100

    fix addEmployeeRate method in EmployeeRateService

--- src/main/java/com/designofficems/designofficemanagementsystem/service/EmployeeRateService.java
+++ src/main/java/com/designofficems/designofficemanagementsystem/service/EmployeeRateService.java
@@ -3,7 +3,6 @@ package com.designofficems.designofficemanagementsystem.service;
 import com.designofficems.designofficemanagementsystem.model.Employee;
 import com.designofficems.designofficemanagementsystem.model.EmployeeRate;
 import com.designofficems.designofficemanagementsystem.repository.EmployeeRateRepository;
-import com.designofficems.designofficemanagementsystem.util.CategoryType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +19,7 @@ public class EmployeeRateService {
     }
 
     public EmployeeRate addEmployeeRate(EmployeeRate employeeRate) {
-        if (employeeRate.getCategory().equals(CategoryType.SALARY)) {
-            return employeeRateRepository.save(employeeRate);
-        } return employeeRateRepository.save(employeeRate);
+        return employeeRateRepository.save(employeeRate);
     }
 
     public List<EmployeeRate> getEmployeeRate(Employee employee) {
