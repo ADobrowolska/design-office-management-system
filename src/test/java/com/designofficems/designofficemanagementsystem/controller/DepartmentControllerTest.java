ed94b0effa7ac7b471d2a7ff7edfb620f1d63b87
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-02-22 19:21:21 +0100

    test: add shouldGetDepartmentByIdWithEmployees test

--- src/test/java/com/designofficems/designofficemanagementsystem/controller/DepartmentControllerTest.java
+++ src/test/java/com/designofficems/designofficemanagementsystem/controller/DepartmentControllerTest.java
@@ -1,12 +1,14 @@
 package com.designofficems.designofficemanagementsystem.controller;
 
 import com.designofficems.designofficemanagementsystem.dto.department.DepartmentDTO;
+import com.designofficems.designofficemanagementsystem.dto.department.DepartmentEmployeeDTO;
 import com.designofficems.designofficemanagementsystem.dto.department.DepartmentMapper;
 import com.designofficems.designofficemanagementsystem.model.Department;
+import com.designofficems.designofficemanagementsystem.model.Employee;
 import com.designofficems.designofficemanagementsystem.repository.DepartmentRepository;
+import com.designofficems.designofficemanagementsystem.repository.EmployeeRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +22,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DepartmentControllerTest extends BaseTest {
@@ -34,6 +36,9 @@ class DepartmentControllerTest extends BaseTest {
     @Autowired
     private DepartmentRepository departmentRepository;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     private Department department;
 
     @BeforeEach
@@ -45,6 +50,15 @@ class DepartmentControllerTest extends BaseTest {
         Department department = new Department();
         department.setName(name);
         return departmentRepository.save(department);
+    }
+
+    private Employee createEmployee(String firstName, String lastName, Department dept) {
+        Employee employee = new Employee();
+        employee.setFirstName(firstName);
+        employee.setLastName(lastName);
+        employee.setDepartment(dept);
+        employee.setUser(getCurrentUser());
+        return employeeRepository.save(employee);
     }
 
     @Test
@@ -81,6 +95,23 @@ class DepartmentControllerTest extends BaseTest {
         assertThat(departmentDTOs.size()).isEqualTo(1);
         assertTrue(departmentDTOs.contains(DepartmentMapper.mapToDepartmentDTO(dept2)));
     }
+
+    @Test
+    void shouldGetDepartmentByIdWithEmployees() throws Exception {
+        Department dept = createDepartment("Road");
+        Employee emp1 = createEmployee("Anna", "Nowak", dept);
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/departments/" + dept.getId() + "/employees")
+                        .headers(getAuthorizedHeader()))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andReturn();
+        DepartmentEmployeeDTO receivedDept = objectMapper.readValue(mvcResult.getResponse().getContentAsString(),
+                DepartmentEmployeeDTO.class);
+        assertThat(receivedDept.getEmployees().size()).isEqualTo(1);
+        assertThat(receivedDept.getName()).isEqualTo("Road");
+    }
+
+
 
 
 
