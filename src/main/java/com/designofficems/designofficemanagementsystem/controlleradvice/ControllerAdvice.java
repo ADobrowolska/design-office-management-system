04b9769034675f729fa43c753f1d4358e5e1dd5e
Aleksandra Dobrowolska <oladobrowolska@outlook.com>
2024-02-26 20:50:33 +0100

    feat: add exception handler - InstanceAlreadyExistsException

--- src/main/java/com/designofficems/designofficemanagementsystem/controlleradvice/ControllerAdvice.java
+++ src/main/java/com/designofficems/designofficemanagementsystem/controlleradvice/ControllerAdvice.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.management.InstanceAlreadyExistsException;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
@@ -13,6 +14,12 @@ public class ControllerAdvice {
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handleNoSuchElementException(NoSuchMethodException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(InstanceAlreadyExistsException.class)
+    public ResponseEntity<String> handleInstanceAlreadyExistsException(InstanceAlreadyExistsException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body(exception.getMessage());
     }
 }
